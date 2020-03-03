32b8a9f90fcbc0d4d4b5723be284c76c1756d018
Imagine Chiu <chihfan.chiu@igroup.tw>
2020-03-04 01:13:11 +0800

    refactor: 優化 刪除沒用到的saga

--- src/library/redux/store/rootSaga.js
+++ src/library/redux/store/rootSaga.js
@@ -9,23 +9,8 @@ import Login from './Login/Saga';
  /** --------------------------------------*/
 const rootSaga = function* root() {
     yield all([
-        // ...require('@library/intl/store/Saga').default,
         ...Startup,
         ...Login,
-        // ...require('./System/Saga').default,
-        // ...require('./Notice/Saga').default,
-        //
-        // ...require('./Home/Saga').default,
-        // ...require('./GameLobby/Saga').default,
-        // ...require('./Promotion/Saga').default,
-        // ...require('./Deposit/Saga').default,
-        //
-        // ...require('./Withdrawal/Saga').default,
-        // ...require('./Profile/Saga').default,
-        // ...require('./Wallet/Saga').default,
-        // ...require('./History/Saga').default,
-        // ...require('./Vip/Saga').default,
-
     ]);
 };
 
