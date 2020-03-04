c7334ff51e16e2f3066ddd31152fe00e1d9e9c32
Imagine Chiu <chihfan.chiu@igroup.tw>
2020-03-04 23:59:00 +0800

    refactor: 優化 不使用mdx進行編輯, 因無法滿足混用的需求, 動態展示無法

---
+++ src/resources/components/atoms/BlockTitle/tests/BlockTitle.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BlockTitle from '../BlockTitle';
+
+export default {
+    title: 'Atoms|BlockTitle',
+};
+
+export const BlockTitleStory = () => <BlockTitle>Title</BlockTitle>;
+
+BlockTitleStory.story = {
+    name: 'default',
+};
+
--- test/storybook/main.js
+++ test/storybook/main.js
@@ -1,14 +1,13 @@
 module.exports = {
     stories: [
-        './examples/*.stories.(js|mdx)',
-        '../../src/resources/components/**/*.stories.(js|mdx)',
+        './examples/*.stories.js',
+        '../../src/resources/components/**/*.stories.js',
     ],
     addons: [
         '@storybook/addon-links/register',
         '@storybook/addon-actions/register',
         '@storybook/addon-viewport/register',
         '@storybook/addon-notes/register-panel',
-        '@storybook/addon-docs',
         'storybook-addon-i18n/register',
     ],
 };
